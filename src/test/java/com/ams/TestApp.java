8542474f71e9ee8d5b448c80ce42ef0ad3e49641
unknown <adhotre@ctepl.com>
2015-08-06 11:15:08 -0700

    added test

---
+++ src/test/java/com/ams/TestApp.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.ams;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author adhotre
+ *
+ */
+public class TestApp {
+	
+	@Test
+	public void test(){
+		Assert.assertTrue(true);
+	}
+
+}
