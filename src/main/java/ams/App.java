58ed83bea22e3cf59403ed0d606b081b526655c4
unknown <adhotre@ctepl.com>
2015-08-09 19:49:45 -0700

    workflow

---
+++ src/main/java/ams/App.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package ams;
+
+/**
+ * App handles all application request
+ * 
+ * @author adhotre
+ *
+ */
+public class App {
+	
+	public void handle(String action){
+		// processing workflow ...
+	}
+	
+
+}
