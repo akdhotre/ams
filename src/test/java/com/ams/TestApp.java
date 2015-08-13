b9d33b6181c2d5ba97a24ca9ac401ea1b999c8c2
unknown <adhotre@ctepl.com>
2015-08-12 22:31:06 -0700

    parameterized test assertion

--- src/test/java/com/ams/TestApp.java
+++ src/test/java/com/ams/TestApp.java
@@ -3,6 +3,9 @@
  */
 package com.ams;
 
+import java.io.BufferedInputStream;
+import java.util.Properties;
+
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -13,8 +16,23 @@ import org.junit.Test;
 public class TestApp {
 	
 	@Test
-	public void test(){
-		Assert.assertTrue(true);
+	public void test(){		
+		boolean testResult = false;
+		try {
+			// after business logic statements assert ...
+			ClassLoader classLoader = getClass().getClassLoader();
+			BufferedInputStream in = (BufferedInputStream) classLoader.getResource("config.properties").getContent();	
+			Properties prop = new Properties();
+			prop.load(in);
+			
+			testResult = Boolean.parseBoolean(prop.getProperty("test.assert"));
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+
+		}
+		Assert.assertTrue(testResult);
+		
 	}
 
 }
